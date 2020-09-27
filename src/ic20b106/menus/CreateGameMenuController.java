2deff84041defb786609a2893b37b53e3583812c
Andre Schneider <schneider.and@gmx.at>
2020-09-27 17:42:54 +0200

    0.0.5 Adds Slider stepping

--- src/ic20b106/menus/CreateGameMenuController.java
+++ src/ic20b106/menus/CreateGameMenuController.java
@@ -1,4 +1,24 @@
 package ic20b106.menus;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
 public class CreateGameMenuController {
+
+    public CreateGameMenuController() {}
+
+    @FXML
+    private void initialize() {
+        // Slider Step from Stackoverflow: https://stackoverflow.com/a/38682583
+        boardWidth.valueProperty().addListener((obs, oldval, newVal) ->
+                boardWidth.setValue((int) Math.round(newVal.doubleValue())));
+        boardHeight.valueProperty().addListener((obs, oldval, newVal) ->
+                boardHeight.setValue((int) Math.round(newVal.doubleValue())));
+    }
+
+    @FXML
+    private Slider boardWidth;
+
+    @FXML
+    private Slider boardHeight;
 }
